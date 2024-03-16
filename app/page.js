791417919805f649aca72b702ce75c0482524bbc
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-03-16 08:00:53 +0530

    changes on 2024-03-16

--- app/page.js
+++ app/page.js
@@ -1,9 +1,35 @@
-import React from "react";
+"use client";
+
+import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
+  // const [currentAccount, setCurrentAccount] = useState('');
+
+  const address = "0x388C818CA8B9251b393131C08a736A67ccB19297";
+
+  const [balance, setBalance] = useState([]);
+
+  const etherJs = async () => {
+    const provider = new ethers.providers.EtherscanProvider();
+    console.log(provider);
+
+    const ethBalance = await provider.getBalance(address);
+
+    console.log("The balance is:" + ethBalance);
+
+    setBalance(ethBalance.toString());
+  };
+
+  useEffect(() => {
+    etherJs();
+  }, []);
+
   return (
     <div className="flex justify-center">
-      <h1 className="font-extrabold text-center text-3xl">Today I am learning Ethers.Js Everything</h1>
+      <h1 className="font-extrabold text-center text-3xl">
+        <p>The Ethers balance : {balance}</p>
+      </h1>
     </div>
   );
 };
